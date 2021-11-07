bbab1851d1dfd203e3887ab85b99c711dc89d61d
Peter Rutschmann <peter.rutschmann@bbw.ch>
2021-11-07 19:12:51 +0100

    Projekt Vorlage erstellen.

---
+++ src/main/java/ch/bbw/pr/moreloops/App.java
@@ -0,0 +1,91 @@
+package ch.bbw.pr.moreloops;
+
+/**
+ * App Klasse
+ *   Anwenden von mehrfachen Loops
+ *   und Kombination von Kontrollelementen.
+ * @author Peter Rutschmann
+ * @author <Ihre Name>
+ * @version 07.11.2021
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Mehrfach-Schleifen" );
+        
+        /* 1. Ausgabe von 1x1 bis 10x10
+         *==========================
+         * Verwenden Sie einen Loop und innerhalb des Loops
+         * einen zweiten Loop.
+         */
+        System.out.println("\n1x1 bis 10x10" );
+        int rows=10;
+        int columns=10;
+        //Hier ergänzen
+
+        /* 2. Ausgabe von Sternen auf einer Zeile
+         *======================================
+         * Verwenden Sie einen Loop, geben Sie 5 * aus.
+         */
+        System.out.println("\n\n5 Sterne:" );
+        int numberOfStars=5;
+        //Hier ergänzen
+
+        /* 3. Ausgabe von einem Dreieck aus *
+         *======================================
+         * Verwenden Sie einen Loop und innerhalb des Loops.
+         * einen zweiten Loop, um eine solche Darstellung zu erzeugen
+         *    *
+         *    **
+         *    ***
+         *    usw.
+         */
+        System.out.println("\n\nDreieck:" );
+        columns=10;
+        //Hier ergänzen
+
+        /* 4. Gleiche Aufgabe wie zuvor
+         *======================================
+         * Verwenden Sie eine andere Loop-Art für die Lösung.
+         *    *
+         *    **
+         *    ***
+         *    usw.
+         */
+        System.out.println("\n\nDreieck, Variante 2:" );
+        columns=10;
+        //Hier ergänzen
+
+
+        /* 5. Dreieck mit Abstand zum Rand
+         *======================================
+         * Verwenden Sie einen Loop und innerhalb des Loops.
+         * einen zweiten Loop, um eine solche Darstellung zu erzeugen
+         * Das Dreieck hat am Anfang "Leerschläge" und ist somit
+         * links abgestuft und rechts bündig.
+         *     *
+         *    **
+         *   ***
+         *    usw.
+         */
+        System.out.println("\n\nDreieck, rechtsbündig:" );
+        columns=10;
+        //Hier ergänzen
+
+        /* 6. Einfacher Tannenbaum
+         *======================================
+         * Verwenden Sie einen Loop und innerhalb des Loops.
+         * einen zweiten Loop, um eine solche Darstellung zu erzeugen
+         *
+         *     *
+         *    ***
+         *   *****
+         *    usw.
+         */
+        System.out.println("\n\nEinfacher Tannenbaum:" );
+        columns=10;
+        //Hier ergänzen
+
+    }
+}
